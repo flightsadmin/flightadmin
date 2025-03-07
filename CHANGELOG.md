1ee7b14d6ec5911665405ee6d272fb06b44ba140
George Chitechi <mctapplications@gmail.com>
2025-03-07 13:04:33 +0300

    Initial Commit

--- CHANGELOG.md
+++
@@ -1,7 +0,0 @@
-# Changelog
-
-All notable changes to `flightadmin` will be documented in this file
-
-## 1.0.0 - 2023-10-10
-
-- initial release
--- resources/install/resources/js/bootstrap.js
+++
@@ -1,9 +0,0 @@
-// Import all of Bootstrap's JS
-import * as bootstrap from 'bootstrap'
-window.bootstrap = bootstrap;
-
-// Import Axios
-import axios from 'axios';
-window.axios = axios;
-
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
